e67aff12671ce65797621befe8c1b33a896ee7bc
CypherNet <cyphernet2001@gmail.com>
2018-05-22 16:34:03 -0300

    paquete controlador y controlador de vistas

---
+++ src/main/java/ar/com/smartprice/controllers/ViewsController.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ar.com.smartprice.controllers;
+
+/**
+ *
+ * @author Andres
+ */
+public class ViewsController {
+
+    /**
+     *
+     * @author Andres
+     * @version 0.1.0
+     *
+     * Esta clase será la encargada de llamar a los servicios y entregar las
+     * solicitudes a las vistas.
+     */
+
+    public static void mainView() {
+        //vista.mainView();
+    }
+
+    public static void registerView() {
+    }
+
+    public static void passwordResetView() {
+    }
+
+    public static void loginView() {
+    }
+
+}
