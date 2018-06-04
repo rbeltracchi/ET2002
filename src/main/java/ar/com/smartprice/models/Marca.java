db4933837c0638806b2eea1a9f064d45016a263a
LuchoIGN <luchoign@gmail.com>
2018-06-04 18:29:48 -0300

    Crear marcas viendo que no existan

--- src/main/java/ar/com/smartprice/models/Marca.java
+++ src/main/java/ar/com/smartprice/models/Marca.java
@@ -7,9 +7,12 @@ package ar.com.smartprice.models;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -17,7 +20,9 @@ import javax.persistence.Lob;
 import javax.persistence.NamedQueries;
 import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
+import javax.persistence.Persistence;
 import javax.persistence.Table;
+import javax.persistence.TypedQuery;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
@@ -33,7 +38,8 @@ import javax.xml.bind.annotation.XmlTransient;
     , @NamedQuery(name = "Marca.findByIdmarcas", query = "SELECT m FROM Marca m WHERE m.idMarca = :idMarca")
     , @NamedQuery(name = "Marca.findByNombre", query = "SELECT m FROM Marca m WHERE m.nombre = :nombre")})
 public class Marca implements Serializable {
-
+    EntityManagerFactory emf= Persistence.createEntityManagerFactory("SmartPricePU");
+        EntityManager em=null;
     @Lob
     @Column(name = "logotipo")
     private byte[] logotipo;
@@ -111,5 +117,32 @@ public class Marca implements Serializable {
     public void setLogotipo(byte[] logotipo) {
         this.logotipo = logotipo;
     }
+    private boolean existMarca(String nombre){
+        if (em==null){
+            em = emf.createEntityManager();
+        }
+        em.getTransaction().begin();
+        TypedQuery<Marca> q;
+        q = em.createNamedQuery("Marca.findByNombre", Marca.class);
+        q.setParameter("nombre", nombre);
+        List<Marca> marcas  = q.getResultList();
+        em.getTransaction().commit();
+        if (marcas.isEmpty())
+            return false;
+        
+        return true;
+    }
     
+    public void insertMarca(Marca mrc){
+       
+      if (!existMarca(mrc.getNombre())) {
+          if (em==null)
+            em = emf.createEntityManager();
+           em.getTransaction().begin();
+           em.persist(mrc);
+           em.getTransaction().commit();
+      }
+      
+      
+    }
 }
