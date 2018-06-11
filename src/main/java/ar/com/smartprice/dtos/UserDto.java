84a57956d05e4880b2e7f2ae9420475080455a50
CypherNet <cyphernet2001@gmail.com>
2018-06-11 20:51:17 -0300

    servicio de login, mapper

--- src/main/java/ar/com/smartprice/dtos/UserDto.java
+++ src/main/java/ar/com/smartprice/dtos/UserDto.java
@@ -40,83 +40,174 @@ public class UserDto {
         this.error = error;
         this.token = token;
     }
-
+    
+    /**
+     * @author Andres 
+     * @return Retorna un {@code String} con el nombre del usuario oferente.
+     */
     public String getName() {
         return name;
     }
-
+    /**
+     * @author Andres
+     * @param  name
+     *         Recibe un {@code String} para setear el nombre a usuario oferente. 
+     */
     public void setName(String name) {
         this.name = name;
     }
-
+    
+    /**
+     * @author Andres 
+     * @return Retorna un {@code String} con el apellido de un usuario oferente
+     */
     public String getLastName() {
         return lastName;
     }
-
+    /**
+     * @author Andres
+     * @param  lastName
+     *         Recibe un {@code String} para setear el apellido a usuario oferente.
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
+    
+    /**
+     * @author Andres 
+     * @return Retorna un {@code String} con el correo de un usuario.
+     */
     public String getEmail() {
         return email;
     }
-
+    
+    /**
+     * @author Andres
+     * @param  email
+     *         Recibe un {@code String} para setear el correo a usuario.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * @author Andres 
+     * @return Retorna un {@code String} con el cuit de un usuario oferente.
+     */
     public String getCuit() {
         return cuit;
     }
 
+    /**
+     * @author Andres
+     * @param  cuit
+     *         Recibe un {@code String} para setear el cuit al usuario oferente.
+     */
     public void setCuit(String cuit) {
         this.cuit = cuit;
     }
 
+    /**
+     * @author Andres 
+     * @return Retorna un {@code String} con la razon social de un usuario oferente.
+     */
     public String getBusinessName() {
         return businessName;
     }
 
+    /**
+     * @author Andres
+     * @param  businessName
+     *         Recibe un {@code String} para setear la razon social al usuario oferente.
+     */
     public void setBusinessName(String businessName) {
         this.businessName = businessName;
     }
-
+    
+    /**
+     * @author Andres 
+     * @return Retorna un {@code String} con la contraseña del usuario.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * @author Andres
+     * @param  password
+     *         Recibe un {@code String} para setear la contraseña al usuario.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
+    /**
+     * @author Andres
+     * @return  Retorna un {@code int} con el numero de tipo de usuario.
+     * <p>1- administrador
+     * <p>2- consumidor
+     * <p>3- oferente
+     */
     public int getUserType() {
         return userType;
     }
 
+    /**
+     * @author Andres
+     * @param  userType
+     *         Recibe un {@code int} para setear el numero de tipo al usuario.
+     */
     public void setUserType(int userType) {
         this.userType = userType;
     }
 
+    /**
+     * @author Andres
+     * @return  Retorna un {@code int} con el numero de id del usuario.
+     */
     public int getUserId() {
         return userId;
     }
 
+    /**
+     * @author Andres
+     * @param  userId
+     *         Recibe un {@code int} para setear el numero de id al usuario.
+     */
     public void setUserId(int userId) {
         this.userId = userId;
     }
-
+    
+    /**
+     * @author Andres
+     * @return  Retorna un {@link ar.com.smartprice.utils.SPError}.
+     */
     public SPError getError() {
         return error;
     }
 
+    /**
+     * @author Andres
+     * @param  error
+     *         Error tipo {@link ar.com.smartprice.utils.SPError}
+     */
     public void setError(SPError error) {
         this.error = error;
     }
 
+     /**
+     * @author Andres 
+     * @return Retorna un {@code String} con el token del usuario.
+     */
     public String getToken() {
         return token;
     }
 
+    /**
+     * @author Andres
+     * @param  token
+     *         Recibe un {@code String} para setear el token al usuario.
+     */
     public void setToken(String token) {
         this.token = token;
     }
--- src/main/java/ar/com/smartprice/models/services/UsersService.java
+++ src/main/java/ar/com/smartprice/models/services/UsersService.java
@@ -1,10 +1,16 @@
-
 package ar.com.smartprice.models.services;
 
 import ar.com.smartprice.dtos.AdministratorDto;
 import ar.com.smartprice.dtos.CredentialsDto;
+import ar.com.smartprice.dtos.TokenInfoDto;
 import ar.com.smartprice.dtos.UserDto;
-
+import ar.com.smartprice.models.Oferente;
+import ar.com.smartprice.models.Users_DBAdmin;
+import ar.com.smartprice.models.Usuario;
+import ar.com.smartprice.utils.Authentication;
+import ar.com.smartprice.utils.Cryptography;
+import ar.com.smartprice.utils.Mapper;
+import ar.com.smartprice.utils.SPError;
 
 /**
  * @author Andres
@@ -18,7 +24,7 @@ public class UsersService {
      * Servicio para la creacion de un usuario.
      *
      * @param user UserDto
-     * @return 
+     * @return
      */
     public boolean create(UserDto user) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.        
@@ -28,19 +34,21 @@ public class UsersService {
      * Servicio para eliminar un usuario.
      *
      * @param user UserDto es el usuario a borrar
-     * @return 
-     * 
+     * @return
+     *
      */
     public static boolean delete(UserDto user) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-    
+
     /**
      * Servicio para eliminar un usuario.
      *
-     * @param user UserDto es el usuario a borrar
-     * @param userRequest usuario que realizar la operacion
-     * @return 
+     * @param user Recibe un {@link ar.com.smartprice.dtos.UserDto} usuario a
+     * borrar
+     * @param userRequest Recibe un {@link ar.com.smartprice.dtos.UserDto}
+     * usuario que realizar la operacion
+     * @return
      */
     public static boolean delete(UserDto user, AdministratorDto userRequest) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -50,7 +58,7 @@ public class UsersService {
      * Servicio para actualizar datos de un usuario.
      *
      * @param user UserDto
-     * @return 
+     * @return
      *
      */
     public static boolean update(UserDto user) {
@@ -61,7 +69,7 @@ public class UsersService {
      * Servicio para obtener un usuario.
      *
      * @param user UserDto
-     * @return 
+     * @return
      *
      */
     public static UserDto read(UserDto user) {
@@ -71,12 +79,48 @@ public class UsersService {
     /**
      * Servicio de autenticacion de usuarios.
      *
-     * @param credentials CredentialsDto credentials
-     * @return UserDto
+     * @param credentials Recibe un
+     * {@link ar.com.smartprice.dtos.CredentialsDto} credentials
+     * @return Retorna un {@link ar.com.smartprice.dtos.UserDto}
      */
     //authentication
     public static UserDto logIn(CredentialsDto credentials) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        UserDto userdto;
+
+        if (credentials.getEmail() == null || credentials.getPassword() == null
+                || credentials.getEmail().isEmpty() || credentials.getPassword().isEmpty()) {
+            return null;
+        }
+
+        Users_DBAdmin usersDb = new Users_DBAdmin();
+
+        Usuario user = usersDb.getByEmail(credentials.getEmail());
+
+        //??
+        //Oferente user = usersDb.getByEmail(credentials.getEmail());
+        if (user == null) {
+            userdto = new UserDto();
+            userdto.setError(new SPError("Usuario sin datos."));
+        } 
+        //verifico si el usuario esta habilitado
+        else if (!user.getActivo()) {
+           
+            userdto = new UserDto();
+            userdto.setError(new SPError("Usuario inhabilitado."));
+        }
+        //comparo passwords
+        else if (!user.getPassword().equals(Cryptography.encrypt(credentials.getPassword(), credentials.getEmail()))) {
+            userdto = new UserDto();
+            userdto.setError(new SPError("Correo o contraseña incorrectos."));
+        } else {
+            //mapper Usuario to UserDto
+
+            userdto = Mapper.UsuarioToUserDto(user);
+
+            //crear token para el usuario
+            userdto.setToken(Authentication.createToken(userdto));
+        }
+        return userdto;
     }
 
     /**
